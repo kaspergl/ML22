6d59108b09a23675f137275531995fd1e8fbd5b1
Kasper Green Larsen <larsen@cs.au.dk>
2022-09-09 11:10:51 +0200

    Fixed OSX data loading issue

--- week2/regression_stumps.py
+++ week2/regression_stumps.py
@@ -10,6 +10,11 @@ import pydotplus
 
 from sklearn.datasets import fetch_california_housing
 
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)): 
+    ssl._create_default_https_context = ssl._create_unverified_context
+
 def plot_tree(dtree, feature_names):
     """ helper function """
     dot_data = StringIO()
