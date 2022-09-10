7f0b8a80c00ee266bcfec19c6038009170e77fd9
Kasper Green Larsen <larsen@cs.au.dk>
2022-09-10 18:11:16 +0200

    Fixed OSX data loading issue

--- week3/linear_regression.py
+++ week3/linear_regression.py
@@ -4,6 +4,12 @@ from sklearn.tree import DecisionTreeRegressor
 
 from sklearn.datasets import fetch_california_housing
 
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)): 
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+
 class LinearRegressor():
 
     def __init__(self):
